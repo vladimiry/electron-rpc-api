a421bd640ea18c5b62700f16651befe881813adb
Vladimir Y <dev.vladimir.y@gmail.com>
2018-12-14 08:44:37 +0300

    support single handler per call channel registering

--- src/lib/ipc-main-api-service.ts
+++ src/lib/ipc-main-api-service.ts
@@ -3,6 +3,12 @@ import {Model, Service} from "pubsub-to-stream-api";
 
 import {AnyType} from "./model";
 
+type RegisterApiIpcMain = Pick<IpcMain, "addListener" | "removeListener" | "emit">;
+type BuildClientIpcRenderer = Pick<IpcRenderer, "on" | "removeListener" | "send">;
+
+const registerApiEventEmitters = new WeakMap<RegisterApiIpcMain, Model.CombinedEventEmitter>();
+const buildClientEventEmitters = new WeakMap<BuildClientIpcRenderer, Model.CombinedEventEmitter>();
+
 export class IpcMainApiService<Api extends Model.ActionsRecord<Extract<keyof Api, string>>> extends Service<Api> {
     public static resolveActionContext(ctx: IpcMainApiActionContext): IpcMainApiActionContext[typeof Model.ACTION_CONTEXT_SYMBOL] {
         return ctx[Model.ACTION_CONTEXT_SYMBOL];
@@ -10,14 +16,25 @@ export class IpcMainApiService<Api extends Model.ActionsRecord<Extract<keyof Api
 
     public registerApi(
         actions: Api,
-        {ipcMain: instance, logger}: { ipcMain?: Pick<IpcMain, "addListener" | "removeListener" | "emit">; logger?: Model.Logger } = {},
+        {
+            ipcMain = require("electron").ipcMain,
+            logger,
+        }: {
+            ipcMain?: RegisterApiIpcMain;
+            logger?: Model.Logger;
+        } = {},
     ) {
-        const ipcMain = instance || require("electron").ipcMain;
-        const em: Model.CombinedEventEmitter = {
-            on: ipcMain.addListener.bind(ipcMain) as AnyType,
-            off: ipcMain.removeListener.bind(ipcMain) as AnyType,
-            emit: ipcMain.emit.bind(ipcMain),
-        };
+        let em = registerApiEventEmitters.get(ipcMain);
+
+        if (!em) {
+            em = {
+                on: ipcMain.addListener.bind(ipcMain) as AnyType,
+                off: ipcMain.removeListener.bind(ipcMain) as AnyType,
+                emit: ipcMain.emit.bind(ipcMain),
+            };
+            registerApiEventEmitters.set(ipcMain, em);
+        }
+
         const requestResolver: Model.RequestResolver = ({sender}, payload) => ({payload, emitter: {emit: sender.send.bind(sender)}});
 
         return this.register(actions, em, {requestResolver, logger});
@@ -25,22 +42,27 @@ export class IpcMainApiService<Api extends Model.ActionsRecord<Extract<keyof Api
 
     public buildClient(
         {
-            ipcRenderer: instance,
+            ipcRenderer = require("electron").ipcRenderer,
             options,
         }: {
-            ipcRenderer?: Pick<IpcRenderer, "on" | "removeListener" | "send">;
+            ipcRenderer?: BuildClientIpcRenderer;
             options?: Model.CallOptions;
         } = {},
     ) {
-        const ipcRenderer = instance || require("electron").ipcRenderer;
-        const em: Model.CombinedEventEmitter = {
-            on: (event, listener) => {
-                ipcRenderer.on(event, (...args: AnyType[]) => listener(args[1]));
-                return em;
-            },
-            off: ipcRenderer.removeListener.bind(ipcRenderer) as AnyType,
-            emit: ipcRenderer.send.bind(ipcRenderer) as AnyType,
-        };
+        let em: Model.CombinedEventEmitter | undefined = buildClientEventEmitters.get(ipcRenderer);
+
+        if (!em) {
+            const newEm: Model.CombinedEventEmitter = {
+                on: (event, listener) => {
+                    ipcRenderer.on(event, (...args: AnyType[]) => listener(args[1]));
+                    return newEm;
+                },
+                off: ipcRenderer.removeListener.bind(ipcRenderer) as AnyType,
+                emit: ipcRenderer.send.bind(ipcRenderer) as AnyType,
+            };
+            buildClientEventEmitters.set(ipcRenderer, newEm);
+            em = newEm;
+        }
 
         return this.caller({emitter: em, listener: em}, options);
     }
--- src/lib/webview-api-service.ts
+++ src/lib/webview-api-service.ts
@@ -1,55 +1,73 @@
-import {IpcRenderer} from "electron";
+import {IpcRenderer, WebviewTag} from "electron";
 import {Model, Service} from "pubsub-to-stream-api";
 
 import {AnyType} from "./model";
 
+type RegisterApiIpcRenderer = Pick<IpcRenderer, "on" | "removeListener" | "sendToHost">;
+
+const registerApiEventEmitters = new WeakMap<RegisterApiIpcRenderer, Model.CombinedEventEmitter>();
+const buildClientEventEmitters = new WeakMap<WebviewTag, Model.CombinedEventEmitter>();
+
 export class WebViewApiService<Api extends Model.ActionsRecord<Extract<keyof Api, string>>> extends Service<Api> {
     public registerApi(
         actions: Api,
-        {ipcRenderer: instance, logger}: {
-            ipcRenderer?: Pick<IpcRenderer, "on" | "removeListener" | "sendToHost">;
+        {
+            ipcRenderer = require("electron").ipcRenderer,
+            logger,
+        }: {
+            ipcRenderer?: RegisterApiIpcRenderer;
             logger?: Model.Logger;
         } = {},
     ) {
-        const ipcRenderer = instance || require("electron").ipcRenderer;
-        const em: Model.CombinedEventEmitter = {
-            on: (event, listener) => {
-                ipcRenderer.on(event, (...args: AnyType[]) => listener(args[1]));
-                return em;
-            },
-            off: ipcRenderer.removeListener.bind(ipcRenderer) as AnyType,
-            emit: (event, ...args) => {
-                ipcRenderer.sendToHost(event, ...args);
-                return true;
-            },
-        };
+        let em = registerApiEventEmitters.get(ipcRenderer);
+
+        if (!em) {
+            const newEm: Model.CombinedEventEmitter = {
+                on: (event, listener) => {
+                    ipcRenderer.on(event, (...args: AnyType[]) => listener(args[1]));
+                    return newEm;
+                },
+                off: ipcRenderer.removeListener.bind(ipcRenderer) as AnyType,
+                emit: (event, ...args) => {
+                    ipcRenderer.sendToHost(event, ...args);
+                    return true;
+                },
+            };
+            registerApiEventEmitters.set(ipcRenderer, newEm);
+            em = newEm;
+        }
 
         return this.register(actions, em, {logger});
     }
 
-    public buildClient(webView: Electron.WebviewTag, {options}: { options?: Model.CallOptions } = {}) {
-        const listenEvent = "ipc-message";
-        const eventEmitter: Model.CombinedEventEmitter = {
-            on: (event, listener) => {
-                webView.addEventListener(listenEvent, ({channel, args}) => {
-                    if (channel !== event) {
-                        return;
-                    }
-                    listener(args[0]);
-                });
-                return eventEmitter;
-            },
-            off: (
-                // @ts-ignore
-                event,
-                listener,
-            ) => {
-                webView.removeEventListener(listenEvent, listener);
-                return eventEmitter;
-            },
-            emit: webView.send.bind(webView) as AnyType,
-        };
+    public buildClient(webView: WebviewTag, {options}: { options?: Model.CallOptions } = {}) {
+        let em = buildClientEventEmitters.get(webView);
 
-        return this.caller({emitter: eventEmitter, listener: eventEmitter}, options);
+        if (!em) {
+            const listenEvent = "ipc-message";
+            const newEm: Model.CombinedEventEmitter = {
+                on: (event, listener) => {
+                    webView.addEventListener(listenEvent, ({channel, args}) => {
+                        if (channel !== event) {
+                            return;
+                        }
+                        listener(args[0]);
+                    });
+                    return newEm;
+                },
+                off: (
+                    // @ts-ignore
+                    event,
+                    listener,
+                ) => {
+                    webView.removeEventListener(listenEvent, listener);
+                    return newEm;
+                },
+                emit: webView.send.bind(webView) as AnyType,
+            };
+            em = newEm;
+        }
+
+        return this.caller({emitter: em, listener: em}, options);
     }
 }
